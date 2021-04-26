a4c10df6cbe854e6932820be90218a4f978ccb89
Tinh <nguuyenvantinhfithou@gmail.com>
2021-04-26 14:11:00 +0700

    d

---
+++ TinhVNP/TinhVNP/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  TinhVNP
+//
+//  Created by Nguyen Van Tinh on 4/26/21.
+//  Copyright © 2021 Nguyen Van Tinh. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    public func showLis(){
+        print("Okay")
+    }
+}
---
+++ TinhVNP/TinhVNP/TinhVNP.h
@@ -0,0 +1,19 @@
+//
+//  TinhVNP.h
+//  TinhVNP
+//
+//  Created by Nguyen Van Tinh on 4/26/21.
+//  Copyright © 2021 Nguyen Van Tinh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TinhVNP.
+FOUNDATION_EXPORT double TinhVNPVersionNumber;
+
+//! Project version string for TinhVNP.
+FOUNDATION_EXPORT const unsigned char TinhVNPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinhVNP/PublicHeader.h>
+
+
